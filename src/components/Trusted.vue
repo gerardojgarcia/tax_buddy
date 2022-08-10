61c6176850a435279a623b58382b6294b38a8b76
Gerardo J Garcia <gerardojgarcia@pm.me>
2022-08-10 12:02:30 -0700

    added title to partners section

--- src/components/Trusted.vue
+++ src/components/Trusted.vue
@@ -17,7 +17,8 @@ components: {
 </script>
 
 <template>
-    <div class="container w-full px-4 flex flex-row flex-wrap ">
+    <div class="container w-full pt-16 px-4 flex flex-col flex-wrap ">
+        <p class="text-lg self-center ">Trusted by these companies</p>
             <div class="trusted-cards w-full flex flex-row flex-wrap items-center" >
             
             <div class="trusted-card  w-2/4 m-0 flex-row flex-wrap  " v-for="partner in partners" :key="partner.id">
